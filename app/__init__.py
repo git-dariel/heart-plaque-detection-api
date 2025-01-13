bfcf7359e72fd74c96240d8534391f3f494afb03
git-dariel <104293978+git-dariel@users.noreply.github.com>
2025-01-13 23:56:01 +0800

    feat: added swagger api docs

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+from flasgger import Swagger
 from app.routes.image_route import image_route
 from app.routes.server_route import server_route
 from app.config.config import Config
 from app.config.cloudinary_config import configure_cloudinary
+from app.config.swagger_config import swagger_config
 
 def create_app():
     app = Flask(__name__)
@@ -16,5 +18,8 @@ def create_app():
 
     # Configure Cloudinary
     configure_cloudinary()
+
+    # Initialize Swagger
+    Swagger(app, config=swagger_config)
 
     return app
